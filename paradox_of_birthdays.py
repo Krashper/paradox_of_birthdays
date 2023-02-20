b3515dab9552d9914def693d5af72bc9fdb7c47e
Solarev Ruslan <ruslan251207@gmail.com>
2023-02-20 20:32:11 +1000

    added numOfBirthdays and demonstration

--- paradox_of_birthdays.py
+++ paradox_of_birthdays.py
@@ -17,3 +17,42 @@ def getMatch(birthdays):
         for birthdayB in birthdays[a + 1:]:
             if birthdayA == birthdayB:
                 return birthdayA
+
+print('''Парадокс дня рождения показывает нам, что в группе из N человек вероятность того, что у двоих из них совпадут дни рождения, на удивление велика. 
+Эта программа выполняет моделирование методом Монте-Карло (то есть повторяющиеся случайные симуляции), чтобы изучить эту концепцию.
+(На самом деле это не парадокс, это просто удивительный результат.)''')
+
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
+while True:
+    print('Как много дней рождений я должен сгенерировать? (Максимально: 100)')
+    response = input('> ')
+    if response.isdecimal() and (0 < int(response) <= 100):
+        numBDays = int(response)
+        break
+print()
+
+print(f'Здесь {numBDays} дней рождений')
+birthdays = getBirthday(numBDays)
+for i, birthday in enumerate(birthdays):
+    if i != 0:
+        print(', ', end='')
+    monthOfBirthday = MONTHS[birthday.month - 1]
+    dayOfBirthday = birthday.day
+    print(f'{monthOfBirthday} {dayOfBirthday}', end='')
+
+print()
+print()
+
+match = getMatch(birthdays)
+print('В этой симуляции, ', end='')
+if match != None:
+    monthOfBirthday = MONTHS[match.month - 1]
+    dateText = f'{monthOfBirthday} {match.day}'
+    print('У нескольких человек день рождения', dateText)
+else:
+    print('У всех людей разные дни рождения')
+print()
+
+
+
